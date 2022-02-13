009aea395756eec3ca3365aa156af64a8b3567bb
Behshad-Moradi <Behshad.Moradi.server@gmail.com>
2022-02-13 13:17:44 +0330

    Append new features to app file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 const prompt = require("prompt-sync")({ sigint: true });
 
-const baseUrl = 'htt://api.kucoin.com'
+const baseUrl = 'https://api.kucoin.com'
 
+//ms
+const sleepTime = 2500;
 
 const sleep = (ms) => {
     return new Promise((resolve, reject) =>{
@@ -19,44 +21,83 @@ async function getData(url){
 
 var tokenName = prompt("Enter cryptocurrency name? ")
 tokenName = tokenName.toUpperCase();
-const url = `/api/v1/market/orderbook/level2_100?symbol=${tokenName}-USDT`
+const orderBookUrl = `/api/v1/market/orderbook/level2_100?symbol=${tokenName}-USDT`;
+const tickerUrl = `/api/v1/market/orderbook/level1?symbol=${tokenName}-USDT`;
+const _24hrStateUrl = `/api/v1/market/stats?symbol=${tokenName}-USDT`;
 
 async function run()
 {
     while (true){
-        const data = getData(url);
-        data.then(response => {
+        const tickerData = getData(tickerUrl);
+        await tickerData.then(response => {
             try{
-                var sum = 0.0;
+                console.log(`${tokenName} Price is: `,response.data.data.price);
+                
+            }catch (err){
+                console.log("There is some error to get ticker...");
+            }
+            
+        })
+        const _24hrState = getData(_24hrStateUrl);
+        await _24hrState.then(response => {
+            try{
+                console.log("Vol(USDT): ", parseInt(response.data.data.volValue));
+            }catch (err) {
+                console.log("There is some error about get 24 hr state");
+            }
+        })   
+        const orderBookData = getData(orderBookUrl);
+        await orderBookData.then(response => {
+            try{
+                var maxB = {"price" : response.data.data.bids[0][0], "amount" : response.data.data.bids[0][1]};
+                var maxS = {"price" : response.data.data.asks[0][0], "amount" : response.data.data.asks[0][1]};
+                    
+                var totalB = 0.0;
                 var length = response.data.data.bids.length;
                 var from = response.data.data.bids[0][0];
                 var to = response.data.data.bids[length - 1][0];
                 for (var i = 0; i < length; i++)
                 {
-                    sum += parseFloat(response.data.data.bids[i][1]);
+                    totalB += parseFloat(response.data.data.bids[i][1]);
+                    
+                    if (parseFloat(response.data.data.bids[i][1]) > maxB.amount)
+                    {
+                        maxB.amount = response.data.data.bids[i][1];
+                        maxB.price = response.data.data.bids[i][0]
+                    }
                 }
                     
-                console.log("Total B: " + parseInt(sum) + " in " + "Range: " + from + ' ~ ' + to);
+                console.log("Total B: " + parseInt(totalB) + " in " + "Range: " + from + ' ~ ' + to);
                     
                 
-                sum = 0.0;
+                totalS = 0.0;
                 length = response.data.data.asks.length;
                 from = response.data.data.asks[0][0];
                 to = response.data.data.asks[length - 1][0];
                 for (var i = 0; i < length; i++)
                 {
-                    sum += parseFloat(response.data.data.asks[i][1]);
+                    totalS += parseFloat(response.data.data.asks[i][1]);
+                    if (parseFloat(response.data.data.asks[i][1]) > maxS.amount)
+                    {    
+                        maxS.amount = response.data.data.asks[i][1];
+                        maxS.price = response.data.data.asks[i][0]
+                    }
                 }
                 
-                console.log("Total S: " + parseInt(sum) + " in " + "Range: " + from + ' ~ ' + to);
+                console.log("Total S: " + parseInt(totalS) + " in " + "Range: " + from + ' ~ ' + to);
+                console.log("Buy - Sell: ", parseInt(totalB - totalS));
+                console.log("Biggest B order: ", maxB);
+                console.log("Biggest S order: ", maxS);
                 
             }catch (err)
             {
                 console.log("There is some error");
             }
-        })
+        });
+        
+        //Next line
         console.log("");
-        await sleep(4500);
+        await sleep(sleepTime);
     }
 }
 run();
